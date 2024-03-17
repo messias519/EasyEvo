5e83aac80167f3b9ac08f209e72a759dbb5d0447
messias519 <messias6@gmail.com>
2024-03-17 17:50:57 -0300

    ajuste img

--- easyevo/src/components/pages/home/home.jsx
+++ easyevo/src/components/pages/home/home.jsx
@@ -10,10 +10,10 @@ const Home = () => {
       </Link>
       <div className="menu">
         <Link to="/calculadora">
-          <img src="./src/components/img/botao-calculadoras.png" alt="Calculadora"/>
+          <img src="/src/components/img/botao-calculadoras.png" alt="Calculadora"/>
         </Link>
         <Link to="/editor">
-          <img src="./src/components/img/botao-editordetexto.png" alt="Editor"/>
+          <img src="/src/components/img/botao-editordetexto.png" alt="Editor"/>
         </Link>
       </div>
      </div>
